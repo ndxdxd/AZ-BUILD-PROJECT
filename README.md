2984898744bf44c5ebeb011c1e2c1068aa1cdd9a
ndxdxd <102645495+ndxdxd@users.noreply.github.com>
2023-12-26 23:31:41 -0800

    calvins code and backend startup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AZ-BUILD-PROJECT
 code for our alarm!
+
+#first update: the fastapi is a past project from matthew, in which we will try to use it as a template to be able to get our hardware to sent post requests to our database
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+
+//express app
+const app = express()
+
+
+//route handler
+app.get('/', (req,res) => {
+    res.json({mssg: 'Welcome to the App'})
+})
+//listen for requests
+app.listen(process.env.PORT, () =>{
+    console.log('listening on port', process.env.PORT)
+}
+)
